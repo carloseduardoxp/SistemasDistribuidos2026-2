ffe750c187bd14b11daafa97be1cb6bfcbb2c544
CArlos Eduardo <carloseduardoxpto@gmail.com>
2026-08-07 19:55:50 -0300

    first commit

---
+++ threads/produtor-consumidor/src/monitor/App.java
@@ -0,0 +1,15 @@
+package monitor;
+
+public class App {
+    public static void main(String[] args) {
+        Buffer buffer = new Buffer();
+
+        Produtor produtor = new Produtor(buffer);
+        Consumidor consumidor = new Consumidor(buffer);
+
+        new Thread(produtor,"Thread Produtora").start();
+        new Thread(consumidor,"Thread Consumidora").start();
+
+        System.out.println("Que os jogos comecem");
+    }
+}
